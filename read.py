1126cf9b286f9fd0641ae77bfc1234fade4abad5
Ayesha <ayeshamulani928@gmail.com>
2024-01-03 19:11:44 +0530

    Initial Commit

---
+++ read.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+
+# #reading images
+# img = cv.imread('Photos/cat.jpg')
+# cv.imshow("Cat", img)
+
+#reading videos
+capture = cv.VideoCapture('Videos/diceVideo.mp4')
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow("Video", frame)
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
